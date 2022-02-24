8d6ecd637b17c32f61857bc7d98c7baf5783a782
VanelS <vsiyou@gmail.com>
2022-02-24 11:20:52 +0100

    Add a notebook for exploratory analysis

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,11 +3,14 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+from sklearn.datasets import fetch_olivetti_faces
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
+
+def load_data():
+    olivetti = fetch_olivetti_faces()
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
